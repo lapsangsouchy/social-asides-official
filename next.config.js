a603f17faa23936280db45c14dcc52954e561e36
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2022-02-09 17:45:42 -0500

    next config update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
   images: {
     domains: [
       'res-1.cloudinary.com',
+      'res-2.cloudinary.com',
+      'res-4.cloudinary.com',
       'static.ghost.org',
       'res-5.cloudinary.com',
       'res-3.cloudinary.com',
