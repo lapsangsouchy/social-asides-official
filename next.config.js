9fee30fc999030e6a1d86dc275df1f81420b236c
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2022-02-09 15:17:45 -0500

    cloudinary config res-3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res-1.cloudinary.com', 'static.ghost.org'],
+    domains: [
+      'res-1.cloudinary.com',
+      'static.ghost.org',
+      'res-5.cloudinary.com',
+      'res-3.cloudinary.com',
+    ],
   },
 };
