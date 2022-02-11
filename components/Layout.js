1328d43043b9f28b64d5588b72d211aa68a81c74
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2022-02-10 19:19:17 -0500

    Head Created

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import Navbar from './Navbar';
 import styles from '../styles/Layout.module.css';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Social Asides</title>
+        <meta
+          property='og:url'
+          content='https://social-asides-official.vercel.app'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Social Asides Blog' />
+        <meta property='og:image' content={'/s-a-cover.png'} />
+      </Head>
       <Navbar />
       <div className={styles.container}>
         <main className={styles.main}>{children}</main>
